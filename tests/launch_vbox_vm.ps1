b83883720e657abffac1acad29b052fa08530e03
TahaSezer <sadecedemise@gmail.com>
2026-03-01 03:39:24 +0300

    mause

--- tests/launch_vbox_vm.ps1
+++ tests/launch_vbox_vm.ps1
@@ -17,7 +17,7 @@ if (Test-Path $vdiPath) {
 
 Write-Host "Creating Virtual Machine..."
 & $vbox createvm --name $vmName --ostype "Linux26_64" --register
-& $vbox modifyvm $vmName --memory 8192 --vram 128 --ioapic on --rtcuseutc on
+& $vbox modifyvm $vmName --memory 8192 --vram 128 --ioapic on --rtcuseutc on --mouse usbtablet
 & $vbox createhd --filename $vdiPath --size 25000 --format VDI
 
 Write-Host "Configuring Storage..."
